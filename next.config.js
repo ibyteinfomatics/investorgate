85f157ef96a20622fbae234c9943eef409252e3d
deepak <deepak@ibyteinfomatics.com>
2022-03-23 15:10:44 +0530

    server commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+    distDir: 'build',
+    reactStrictMode: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      loader: 'imgix',
+      path: '/',
+    }
+  }
